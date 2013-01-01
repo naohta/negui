10cee735db92b0be874a07d52afb600d27b028c0
Naohiro OHTA <naohta@gmail.com>
2013-01-02 02:08:07 +0900

    Refactored. They have good URL now.

--- web.rb
+++ web.rb
@@ -13,7 +13,7 @@ def pad(s)
   "pad([" + s + "])"
 end
 
-get '/read/tables' do
+get '/read' do
   content_type:json
   tables = Dynamodb.db.tables
   s="";first=true;tables.each{ |t|
@@ -23,46 +23,36 @@ get '/read/tables' do
   pad(s)
 end
 
-get '/read/table/:table_name' do
+def json_from_item(item)
+  if(item.attributes.count==0) then return '' end
+  s = '{';
+  first=true; item.attributes.each{ |attr|
+    if(first) then first=false else s+=',' end
+    key=attr[0]; value=attr[1]
+    s += '"' + key + '":"'
+    s += (value.class==BigDecimal) ? value.to_i.to_s : value
+    s += '"'
+  }
+  s += '}'
+end
+
+get '/read/:table_name' do
   content_type :json
   tbl = Dynamodb.db.tables[params[:table_name]]
   tbl.load_schema
   items = tbl.items
   s="";firstA=true;items.each{ |item|
-	if(firstA) then firstA=false else s+="," end
-	s += '{'
-    firstB=true; item.attributes.each{ |attr|
-      if(firstB) then firstB=false else s+=',' end
-      s += "\"#{attr[0]}\":"
-      if(attr[1].class==BigDecimal) then
-        s += "\"#{attr[1].to_i}\""
-      else
-        s += "\"#{attr[1]}\""
-      end
-    }
-    s += '}'
+    if(firstA) then firstA=false else s+="," end
+    s += json_from_item(item)
   }
   pad(s)
 end
 
-get '/read/item/:table_name/:hash_key' do
+get '/read/:table_name/:hash_key' do
   content_type :json
   tbl = Dynamodb.db.tables[params[:table_name]]
   tbl.load_schema
-  item = tbl.items[params[:hash_key]]
-  p item.attributes.count
-  s = '{'; firstB=true; item.attributes.each{ |attr|
-    p attr
-    if(firstB) then firstB=false else s+=',' end
-    s += "\"#{attr[0]}\":"
-    if(attr[1].class==BigDecimal) then
-      s += "\"#{attr[1].to_i}\""
-    else
-      s += "\"#{attr[1]}\""
-    end
-  }
-  s += '}'
-  p s
-  p pad(s)
-  pad(s)    
+  pad(json_from_item(tbl.items[params[:hash_key]]))    
 end
+
+
