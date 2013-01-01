6315b05a081388989a114c128c0ffb88a7960586
Naohiro OHTA <naohta@gmail.com>
2013-01-01 15:03:38 +0900

    Yeah, WORK!

--- web.rb
+++ web.rb
@@ -13,20 +13,7 @@ def pad(s)
   "pad([" + s + "])"
 end
 
-def jsonp(list,sub_name)
-  s=""; first=true; list.each{ |i|
-    if(first) then first=false else s+="," end
-    s += JSON.generate(eval("i."+sub_name))
-    #p AWS::DynamoDB
-    #p AWS::Core::Options::JSONSerializer
-	#j = AWS::Core::Options::JSONSerializer.new(nil,nil)
-	#p j.serialize!(eval("i."+sub_name))
-    p (eval("i."+sub_name)).class
-  }
-  return pad(s)
-end
-
-get '/read' do
+get '/read/tables' do
   content_type:json
   tables = Dynamodb.db.tables
   s="";first=true;tables.each{ |t|
@@ -36,34 +23,46 @@ get '/read' do
   pad(s)
 end
 
-get '/read/:table' do
+get '/read/table/:table_name' do
   content_type :json
-  tbl = Dynamodb.db.tables[params[:table]]
+  tbl = Dynamodb.db.tables[params[:table_name]]
   tbl.load_schema
-  items = tbl.items()
-  jsonp(items,"attributes.to_h")
-end
-
-get '/read2/:table' do
-  content_type :json
-  tbl = Dynamodb.db.tables[params[:table]]
-  tbl.load_schema
-  items = tbl.items()
-  s="";first=true;items.each{ |item|
-	if(first) then first=false else s+="," end
+  items = tbl.items
+  s="";firstA=true;items.each{ |item|
+	if(firstA) then firstA=false else s+="," end
 	s += '{'
-    first2=true; item.attributes.each{ |attr|
-      if(first2) then first2=false else s+=',' end
+    firstB=true; item.attributes.each{ |attr|
+      if(firstB) then firstB=false else s+=',' end
       s += "\"#{attr[0]}\":"
       if(attr[1].class==BigDecimal) then
         s += "\"#{attr[1].to_i}\""
       else
         s += "\"#{attr[1]}\""
       end
-      p attr[1].class
-      p attr
     }
     s += '}'
   }
   pad(s)
+end
+
+get '/read/item/:table_name/:hash_key' do
+  content_type :json
+  tbl = Dynamodb.db.tables[params[:table_name]]
+  tbl.load_schema
+  item = tbl.items[params[:hash_key]]
+  p item.attributes.count
+  s = '{'; firstB=true; item.attributes.each{ |attr|
+    p attr
+    if(firstB) then firstB=false else s+=',' end
+    s += "\"#{attr[0]}\":"
+    if(attr[1].class==BigDecimal) then
+      s += "\"#{attr[1].to_i}\""
+    else
+      s += "\"#{attr[1]}\""
+    end
+  }
+  s += '}'
+  p s
+  p pad(s)
+  pad(s)    
 end
