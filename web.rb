f4dddfaad5b0f66c22cee4b8fb8c5f7e6a83f372
Naohiro OHTA <naohta@gmail.com>
2013-01-04 01:41:43 +0900

    mmm..

--- web.rb
+++ web.rb
@@ -8,6 +8,9 @@ get '/hello/:name' do "hello, #{params[:name]}:)" end
 get '/test/json' do 'pad(["some1","some2","some3"]);' end
 get '/test/json2' do 'pad([{"root":{"k1":"yas!","k2":"This is JSON one."}}])' end
 get '/test/json3' do 'pad([{"k1":"v1"},{"k2":"v2"},{"k3":"v3"}])' end
+get '/test/params/*:*' do |a,b|
+  p a,b
+end
 
 def pad(s)
   "pad([" + s + "])"
@@ -55,4 +58,22 @@ get '/read/:table_name/:hash_key' do
   pad(json_from_item(tbl.items[params[:hash_key]]))    
 end
 
+get '/write/:table_name/:hash_key' do
+  content_type:json
+  tbl = Dynamodb.db.tables[params[:table_name]]
+  tbl.load_schema
+  p :hash_key
+  p "#{:hash_key}"
+  #item = tbl.items.create('id'=> params[:hash_key])
+  item = tbl.items.create(id:params[:hash_key])
+end
+
+
+get '/write2/:table_name/:hash_of_hash_key' do
+  content_type:json
+  tbl = Dynamodb.db.tables[params[:table_name]]
+  tbl.load_schema
+  p params[:hash_of_hash_key]
+  item = eval("tbl.items.create(#{params[:hash_of_hash_key]})")
+end
 
