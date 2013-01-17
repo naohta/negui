943c38fc42d33e82dab137abe35434c149dd8a44
naohta <naohta@gmail.com>
2013-01-17 15:15:03 +0900

    delete some unused lines

--- web.rb
+++ web.rb
@@ -1,23 +1,28 @@
 #encoding:utf-8
 require 'sinatra'
 def load_backyard
-  load './backyard_with_dynamo.rb'
+  load './backyard.rb'
 end
 load_backyard
 
-get '/' do "Hello!" end
 get '/hello/:name' do "hello, #{params[:name]}:)" end
 get '/test/json' do 'pad(["some1","some2","some3"]);' end
 get '/test/json2' do 'pad([{"root":{"k1":"yas!","k2":"This is JSON one."}}])' end
 get '/test/json3' do 'pad([{"k1":"v1"},{"k2":"v2"},{"k3":"v3"}])' end
 get '/test/params/*:*' do |a,b| "Key is '#{a}', value is '#{b}'." end
 
+
+before do
+  content_type:json
+end
+
+
+get '/read' do read_table_names end
 get '/load' do load_backyard; "Backyard programs are loaded." end
-get '/read' do content_type:json; read_table_names end
-get '/new' do content_type:json; new_absence end
-get '/list/*' do |s| content_type:json; list_notices s; end
-get '/time' do content_type:json; time end
-get '/template/*' do |title| content_type:json; template title; end
-get '/new2/*' do |title| content_type:json; submit_notice(title); end
+get '/time' do time end
 
 
+get '/list/*' do |s| list_notices s end
+get '/submit/*' do |title| submit_notice(title) end
+
+get '/template/*' do |title| template title end
